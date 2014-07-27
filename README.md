6b0f5c367e686866d5bd4c0cc3f7c0ee19cfaf02
Thomas Watson Steen <w@tson.dk>
2014-07-27 20:10:32 +0200

    All your base are belong to us

--- README.md
+++ README.md
@@ -1,4 +1,71 @@
-airharvest
-==========
+# AirHarvest
 
-A tool to scan your network for RAOP and AirPlay bonjour services and send their details to an open central repository
+The Apple AirPlay and RAOP protocols are the foundation of AppleTV and
+all AirPlay compatible hardware. The two protocols are continuesly
+evolving and new features are added. Clément Vasseur did [a fantastic
+job documenting](http://nto.github.io/AirPlay.html) most of the two
+protcols in 2012, but since then a lot have happended.
+
+This project is a first step in trying to fully uncover the two
+protocols.
+
+Installing an running this software on your computer will collect and
+send information about all AirPlay and RAOP compatible devices on your
+network **to a public database**. From this data and a lot of hard work
+it's my hope that we can reverse engineer the two protocols.
+
+## Installation
+
+This is a Node.js module and requires the [Node.js package
+manager](https://www.npmjs.org) to install.
+
+```
+npm install -g airharvest
+```
+
+## Run it!
+
+Just run the `airharvest` command from your command line :)
+
+## What does the collected data look like?
+
+This is an example of what kind of RAOP data is collected:
+
+```json
+{
+    "type": "raop",
+    "port": 5000,
+    "txt": {
+        "cn": "0,1,2,3",
+        "da": "true",
+        "et": "0,3,5",
+        "ft": "0x5A7FFFF7,0xE",
+        "md": "0,1,2",
+        "am": "AppleTV3,1",
+        "pk": "****************************************************************",
+        "sf": "0x44",
+        "tp": "UDP",
+        "vn": "65537",
+        "vs": "200.54",
+        "vv": "2"
+    }
+}
+```
+
+This is an example of what kind of AirPlay data is collected:
+
+```json
+{
+    "type": "airplay",
+    "port": 7000,
+    "txt": {
+        "deviceid": "***",
+        "features": "0x5A7FFFF7,0xE",
+        "flags": "0x44",
+        "model": "AppleTV3,1",
+        "pk": "****************************************************************",
+        "srcvers": "200.54",
+        "vv": "2"
+    }
+}
+```
---
+++ index.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var fs = require('fs');
+var crypto = require('crypto');
+var http = require('http');
+var mdns = require('mdns');
+
+var found = false;
+var cbCount = 0;
+
+var isKnown = function (service, callback) {
+  var key = getKey(service);
+  console.log('Found an %s service with hash %s - verifying...', service.type, key);
+  var req = http.request({
+    method: 'HEAD',
+    host: 'mindus.iriscouch.com',
+    path: '/airharvest/' + key
+  });
+  req.on('response', function (res) {
+    cbCount--;
+    if (res.statusCode === 200) callback(null, true);
+    else callback(null, false);
+  });
+  req.on('error', callback);
+  req.end();
+  cbCount++;
+};
+
+var insert = function (service, callback) {
+  var key = getKey(service);
+  var json = JSON.stringify(service);
+  var req = http.request({
+    method: 'PUT',
+    host: 'mindus.iriscouch.com',
+    path: '/airharvest/' + key,
+    headers: {
+      'Content-Length': json.length,
+      'Content-Type': 'application/json'
+    }
+  });
+  req.on('response', function (res) {
+    cbCount--;
+    if (res.statusCode === 201) callback();
+    else callback(new Error('Unexpected response from the database: ' + res.statusCode));
+  });
+  req.on('error', callback);
+  req.end(json);
+  cbCount++;
+};
+
+var filter = function (service) {
+  return {
+    type: service.type.name,
+    port: service.port,
+    txt: service.txtRecord
+  };
+};
+
+var anon = function (service) {
+  if ('deviceid' in service.txt) service.txt.deviceid = '***';
+  if ('pk' in service.txt) service.txt.pk = service.txt.pk.replace(/./g, '*');
+  return service;
+};
+
+var getKey = function (service) {
+  var md5sum = crypto.createHash('md5');
+  md5sum.update(JSON.stringify(service));
+  return md5sum.digest('hex');
+};
+
+var save = function (service) {
+  service = anon(filter(service));
+  isKnown(service, function (err, known) {
+    if (err) throw err;
+    if (known) {
+      console.log('Found known configuration - skipping...');
+      done();
+      return;
+    }
+
+    found = true;
+    console.log('Found unknown configuration - publishing data...');
+    insert(service, function (err) {
+      if (err) throw err;
+      console.log('Successfully stored the new configuration');
+      done();
+    });
+  });
+};
+
+var done = function () {
+  if (cbCount) return;
+  if (found)
+    console.log('\nWow! You discovered new configurations! - You deserved a nice cold beer :)');
+  else
+    console.log('\nDid not find anything new on your network - Thanks for being awesome! :)');
+  process.exit();
+};
+
+console.log('Analyting network...');
+mdns.createBrowser(mdns.tcp('raop')).on('serviceUp', save).start();
+mdns.createBrowser(mdns.tcp('airplay')).on('serviceUp', save).start();
